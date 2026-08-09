f0b63d76d0e018e0a949b3faea5e74ff90b6b47c
Samriti Bhatia <samritibhatia7871@gmail.com>
2026-08-08 21:25:08 -0400

    Update 01_create_tables.sql

--- schema/01_create_tables.sql
+++ schema/01_create_tables.sql
@@ -1,2 +1,12 @@
 -- Restaurant Database
 -- Database schema and table creation
+
+
+Create Table  Customers (
+  customer_id Integer Primary Key AUTOINCREMENT, 
+  customer_name TEXT NOT NULL , 
+  Phone_Number TEXT UNIQUE, 
+  email TEXT UNIQUE , 
+  city TEXT, 
+  craeted_at DATE Default  Current_Date 
+);
