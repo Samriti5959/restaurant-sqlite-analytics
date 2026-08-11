4c3adcd569aff6408e010b4ccb89d0576c4088c3
Samriti Bhatia <samritibhatia7871@gmail.com>
2026-08-11 19:26:43 -0400

    Update 01_create_tables.sql

--- schema/01_create_tables.sql
+++ schema/01_create_tables.sql
@@ -16,3 +16,13 @@ Create Table Categories (
   category_id Integer Primary Key AUTOINCREMENT,
   category_name TEXT UNIQUE NOT NULL
   );
+
+---Menu Items 
+Create Table MenuItems (
+item_id Integer Primary Key Auto Increment ,
+item_name Text NOT NULL ,
+category_id Integer NOT NULL,
+price  value NOT NULL
+is_available Integer DEFAULT 1
+category_id ko Categories table ke category_id se link karna hai
+
